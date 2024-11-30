56a4566f2e78c5475cd315d1343b5c7432ca7115
John Doe <johndoe@email.com>
2024-11-30 21:16:56 +0700

    setup product and product detail client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter as Router , Routes, Route , Navigate } from 'react-router-dom'
+import ProductDetail from './pages/ProductDetail.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route  path="/" element={<App />} />
+        <Route  path="/detail" element={<ProductDetail />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
